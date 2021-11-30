06b36645795b3d8eafd27c137391014fb468c561
Kevin Guerrero <guerrk83z@gmail.com>
2021-11-29 19:50:58 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("#create-task-form")
+
+  //form.addEventListener("submit", (e) => e.preventDefault())
+  form.addEventListener("submit", function (e) {
+    //console.log(e.target.new_task_description.value)
+    handleSubmit(e.target.new_task_description.value)
+    e.preventDefault()
+  }) 
+
 });
+
+
+function handleSubmit(todo) {
+  let p = document.createElement("p")
+  let button = document.createElement("button")
+  button.addEventListener("click", handleDelete)
+  p.textContent = `${todo}  `
+  button.textContent = "x"
+  document.querySelector("#tasks").append(p)
+  p.appendChild(button)
+}
+
+function handleDelete (e) {
+  e.target.parentNode.remove()
+}
+
+
+// const form = document.querySelector("#create-task-form")
+
+// form.addEventListener("submit", function (e) {
+//   e.preventDefault()
+// })
